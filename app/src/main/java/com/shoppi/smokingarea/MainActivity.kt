def41d3f6b4da012c7f3984547a14fd834292358
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-11 23:16:10 +0900

    [2] Set a SplashActivity and BottomNav

--- app/src/main/java/com/shoppi/smokingarea/MainActivity.kt
+++ app/src/main/java/com/shoppi/smokingarea/MainActivity.kt
@@ -2,10 +2,22 @@ package com.shoppi.smokingarea
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        val navController = navHostFragment?.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        if (navController != null) {
+            bottomNavigationView.setupWithNavController(navController)
+        }
+
     }
 }
