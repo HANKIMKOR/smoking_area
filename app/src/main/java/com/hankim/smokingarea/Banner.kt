90ecb2a830f66a112b5aaae37fc68c2e2e477a6d
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-21 19:29:43 +0900

    [8] Json... map view

---
+++ app/src/main/java/com/hankim/smokingarea/Banner.kt
@@ -0,0 +1,12 @@
+package com.hankim.smokingarea
+
+data class Banner(
+    val local1: String,
+    val local2: String,
+    val place: String,
+    val address: String,
+    val sector: String,
+    val divide1: String,
+    val divide2: String,
+    val update: String,
+)
---
+++ app/src/main/java/com/hankim/smokingarea/SearchBannerAdapter.kt
@@ -0,0 +1,51 @@
+package com.hankim.smokingarea
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+
+class SearchBannerAdapter :
+    ListAdapter<Banner, SearchBannerAdapter.SearchBannerViewHolder>(BannerDIffCallback()) {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchBannerViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_search_banner, parent, false)
+        return SearchBannerViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SearchBannerViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    class SearchBannerViewHolder(view: View) : RecyclerView.ViewHolder() {
+
+        private val bannerView = view.findViewById<View>(R.id.v_banner)
+        private val bannerPlaceNameTextView = view.findViewById<TextView>(R.id.tv_place_name)
+        private val bannerPlaceAddressTextView = view.findViewById<TextView>(R.id.tv_place_address)
+        private val bannerPlaceSearchTextView = view.findViewById<TextView>(R.id.tv_place_search)
+
+
+
+        fun bind(banner: Banner) {
+
+
+        }
+    }
+}
+
+class BannerDIffCallback : DiffUtil.ItemCallback<Banner>() {
+    override fun areItemsTheSame(oldItem: Banner, newItem: Banner): Boolean {
+        return oldItem.address == newItem.address
+    }
+
+    override fun areContentsTheSame(oldItem: Banner, newItem: Banner): Boolean {
+        return oldItem == newItem
+    }
+
+}
