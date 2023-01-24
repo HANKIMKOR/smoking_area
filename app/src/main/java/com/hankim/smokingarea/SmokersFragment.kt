6e80e521d98c8c848b3f0f0e9cc524bb72851597
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2023-01-25 01:22:51 +0900

    [12] Map marker and recyclerView(2)...!!!!

--- app/src/main/java/com/hankim/smokingarea/SmokersFragment.kt
+++ app/src/main/java/com/hankim/smokingarea/SmokersFragment.kt
@@ -1,6 +1,5 @@
 package com.hankim.smokingarea
 
-import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,22 +8,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hankim.smokingarea.network.ApiClient
-import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.*
-import com.naver.maps.map.overlay.Marker
-import com.naver.maps.map.overlay.OverlayImage
-import com.naver.maps.map.util.FusedLocationSource
-import com.naver.maps.map.util.MarkerIcons
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class SmokersFragment : Fragment(), OnMapReadyCallback {
-    private lateinit var naverMap: NaverMap
-    private lateinit var locationSource: FusedLocationSource
-    private lateinit var mapView: MapView
+class SmokersFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
     private var recyclerAdapter = SmokersListAdapter()
@@ -36,12 +26,6 @@ class SmokersFragment : Fragment(), OnMapReadyCallback {
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_smokers, container, false)
 
-        mapView = rootView.findViewById(R.id.mapView) as MapView
-        mapView.onCreate(savedInstanceState)
-        mapView.getMapAsync(this)
-
-
-
         recyclerView = rootView.findViewById(R.id.recyclerView)
         recyclerView.adapter = recyclerAdapter
         recyclerView.layoutManager = LinearLayoutManager(context)
@@ -49,10 +33,8 @@ class SmokersFragment : Fragment(), OnMapReadyCallback {
         return rootView
     }
 
-    override fun onMapReady(map: NaverMap) {
-        naverMap = map
-        locationSource = FusedLocationSource(this@SmokersFragment, LOCATION_PERMISSION_REQUEST_CODE)
-        naverMap.locationSource = locationSource
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         getSmokingListFromAPI()
     }
@@ -65,7 +47,7 @@ class SmokersFragment : Fragment(), OnMapReadyCallback {
 
         retrofit.create(ApiClient::class.java).also {
             it.getSmokingList()
-                .enqueue(object : Callback<SearchData>{
+                .enqueue(object : Callback<SearchData> {
                     override fun onResponse(
                         call: Call<SearchData>,
                         response: Response<SearchData>
@@ -74,8 +56,7 @@ class SmokersFragment : Fragment(), OnMapReadyCallback {
                             return
                         }
 
-                        response.body()?.let {data ->
-                            updateMarker(data.smokingList)
+                        response.body()?.let { data ->
                             recyclerAdapter.submitList(data.smokingList)
                         }
                     }
@@ -86,76 +67,5 @@ class SmokersFragment : Fragment(), OnMapReadyCallback {
 
                 })
         }
-    }
-
-    private fun updateMarker(dataLists: List<SmokingList>) {
-        dataLists.forEach {dataList ->
-            val marker = Marker()
-            marker.position = LatLng(dataList.lat, dataList.lng)
-            // todo 마커 클릭 리스터
-            marker.map = naverMap
-            marker.tag = dataList.id
-            marker.icon = OverlayImage.fromResource(R.drawable.ic_smoking_marker)
-
-        }
-    }
-
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        if (locationSource.onRequestPermissionsResult(
-                requestCode, permissions,
-                grantResults
-            )
-        ) {
-            if (!locationSource.isActivated) { // 권한 거부됨
-                naverMap.locationTrackingMode = LocationTrackingMode.None
-            }
-            return
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-
-    override fun onStart() {
-        super.onStart()
-        mapView.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onResume()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView.onLowMemory()
-    }
-
-    companion object {
-        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
